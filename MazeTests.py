8c5cffa34c49b686dce2342922e787e0ca902e35
Your Name <AlexSempervive@alexs-MacBook-Pro-5.local>
2016-09-07 20:41:04 -0400

    testing for turtle

--- MazeTests.py
+++ MazeTests.py
@@ -1,14 +1,40 @@
 import unittest
 from Maze import *
 
-class testMaze (unittest.TestCase):
-    def setUp (self):
-        # this checks for maze class
-        self.m=Maze()
+class testMaze(unittest.TestCase):
+    """
+    This class inherits from a class called TestCase which is
+    defined in the unittest moduel.
 
-    def testScreenExists (self):
-        assert type (self.m) == Maze
+    When you inherit from a class, you get all the methods that are
+    defined in that class for free.
 
-if __name__=="__main__":
-    unittest.main(exit=False)
+    Since this is a TestCase class, calling unittest.main() will automatically
+    run any of the functions that start with the word 'test'.
+    """
+    
+    def setUp(self):
+        """
+        The setUp function is called each time one of your tests is run.
+        We create an instance of the maze here before each test."
+        """
+        # this checks for a Maze class
+        self.m = Maze()
 
+    def testMazeExists(self):
+        """
+        This will check to see if we have a maze class but as soon
+        as setUp is run, we will seee a failure so we really don't need
+        to do anything here
+        """
+        pass
+    
+    def testScreenExists(self):
+        assert type(self.m.s) == turtle._Screen
+
+    def testTurtleExists(self):
+        assert type(self.m.t) == turtle._Turtle
+        
+
+
+unittest.main()
