5fa275a5ee85c40322b93414effe62403d17c116
alex Sempervive <AlexSempervive@alexs-MacBook-Pro-5.local>
2016-09-07 20:19:15 -0400

    first commit

---
+++ MazeTests.py
@@ -0,0 +1,14 @@
+import unittest
+from Maze import *
+
+class testMaze (unittest.TestCase):
+    def setUp (self):
+        # this checks for maze class
+        self.m=Maze()
+
+    def testScreenExists (self):
+        assert type (self.m) == Maze
+
+if __name__=="__main__":
+    unittest.main(exit=False)
+
